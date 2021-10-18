ab6627f6e973a80b0dec6c94bd3987bdd79504fe
Ritesh Kapoor <ritesh@workoidz.com>
2021-10-18 18:31:08 +0530

    association ready

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+	has_many :followed_users, foreign_key: :follower_id , class_name: "Connection"
+    has_many :followed, through: :followed_users
+
+    has_many :following_users, foreign_key: :followed_id, class_name: "Connection"
+    has_many :followers, through: :following_users
+
+end
